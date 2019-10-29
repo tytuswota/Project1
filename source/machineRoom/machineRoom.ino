230c571f1e59f585b2ab7c21b116a1a38a150b97
tytuswota <wabijack@gmail.com>
2019-10-29 17:18:18 +0100

    other motor code

--- source/machineRoom/machineRoom.ino
+++ source/machineRoom/machineRoom.ino
@@ -1,9 +1,31 @@
-#include <Stepper.h>
 #include <Wire.h>
 
 #define CHAR_RECEIVE_AMOUNT 4
 
 char buffer[CHAR_RECEIVE_AMOUNT];
+
+class ControlledMotor {
+  public:
+  int pin1, pin2;
+  ControlledMotor(int pin1, int pin2) {
+    this->pin1 = pin1;
+    this->pin2 = pin2;
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+  }
+  void spinUp() {
+    digitalWrite(pin1, 1);
+    digitalWrite(pin2, 0);
+  }
+  void spinDown() {
+    digitalWrite(pin1, 0);
+    digitalWrite(pin2, 1);
+  }
+  void stop() {
+    digitalWrite(pin1, 0);
+    digitalWrite(pin2, 0);
+  }
+};
 
 const int stepsPerRevolution = 64;
 int curFloor = 0;
@@ -13,13 +35,12 @@ int mFloor;
 int req = 0;
 int mDestination;
 int transmissionAddress = 0;
-int index = 0;
-//0 is down 1 is up
 int liftDirection = 0;
 int reqDirection = 0;
 
 
-Stepper motor(stepsPerRevolution, 8, 9, 10, 11);
+//Stepper motor(stepsPerRevolution, 8, 9, 10, 11);
+ControlledMotor motor(5, 6);
 
 int buttonstate1 = 0;
 int buttonstate0 = 0;
@@ -27,16 +48,13 @@ int buttonstate0 = 0;
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  motor.setSpeed(15);
 }
 
-void loop() {
+void loop() {  
     Wire.beginTransmission(transmissionAddress);
-    //Serial.println(curFloor);
     Wire.write(curFloor);
     Wire.endTransmission();
-
-    //Serial.println(index);
+    
     Wire.requestFrom(transmissionAddress, CHAR_RECEIVE_AMOUNT);
     memset(buffer, 0, CHAR_RECEIVE_AMOUNT);
     int i = 0;
@@ -61,10 +79,6 @@ void loop() {
       case 2:
         Serial.print("Detection on floor ");
         curFloor = mFloor;
-        if(index > 0 )
-        {
-         index--; 
-        }
         mAction = 0;
         break;
       case 3:
