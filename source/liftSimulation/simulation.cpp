d5854221cb9b9f1f1c69e03aaf9ba1d0dc494a6f
Don Luyendijk <donluyendijk@gmail.com>
2019-10-30 13:52:04 +0100

    small change no. #5237398475

--- source/liftSimulation/simulation.cpp
+++ source/liftSimulation/simulation.cpp
@@ -13,6 +13,7 @@ typedef union req {
     int data[3];
 }req;
 
+// function to turn a direction in a char for printing
 char dir_to_char(int dir) {
     char arr[] = "v-^";
     return arr[dir+1];
@@ -25,6 +26,7 @@ class Elevator {
     int prevreqsrc;
     req requests[4];
     
+    // constructor sets all request data to zeroes
     Elevator() {
         for(int i = 0; i < 4; i++) {
             for(int j = 0; j < 4; j++) {
@@ -33,28 +35,42 @@ class Elevator {
         }
     }
 
+    // function to go to the next floor
     void next() {
         if(floor == 1) direction = UP;
         else if(floor == 4) direction = DOWN;
         floor += direction;
         printf("lift going %c, now on floor %d\n", dir_to_char(direction), floor);
+        handle_stops();
+    }
+
+    // functon to handle stopping
+    void handle_stops() {
+
+        // check if there is a request with the current elevator floor as source
         for(int i = 0; i < 4; i++) {
             if(requests[i].source == floor) {
+
+                // set the request to 'passed'
                 requests[i].stop_passed = 1;
             }
         }
-        update();
-    }
 
-    void update() {
+        // check if there's a (passed) stop request or request to go in the current direction of the elevator
         if(requests[floor].type == direction || (requests[floor].type == STOP && requests[floor].stop_passed)) {
+            
+            // handle the request
             requests[floor].type = NONE;
             printf("stopping at floor %d\n", floor);
         }
     }
 
+    // function to make a request
     void make_request(int floor, int dir) {
         requests[floor].type = dir;
+
+        // if the request is a stop request, set the source as the floor from
+        // the previous request
         if(dir == STOP) {
             requests[floor].source = prevreqsrc;
         }
@@ -68,7 +84,7 @@ int main() {
     // start at floor 0
     // should be set to the floor sending a detection
     // if no floor is sending a detection, go (up/down?) until a floor is sending a detection
-    elevator.floor = 0;
+    elevator.floor = 1;
 
     // start by going down if the current floor is 4, else start by going up
     if(elevator.floor == 4) {
