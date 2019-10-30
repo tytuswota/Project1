989eef0d2fc9404fd0d9b036ec8b0b4227aee286
Don Luyendijk <donluyendijk@gmail.com>
2019-10-30 15:47:30 +0100

    significant update to simulation

--- source/liftSimulation/simulation.cpp
+++ source/liftSimulation/simulation.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define UP 1
 #define STOP 2
 
+// req has a type, a source and a variable to see if the source has been passed
 typedef union req {
     struct {
         int type, source, stop_passed;
@@ -13,20 +14,21 @@ typedef union req {
     int data[3];
 }req;
 
-// function to turn a direction in a char for printing
+// function to turn a direction into a char for printing
 char dir_to_char(int dir) {
-    char arr[] = "v-^";
+    char arr[] = "v-^x";
     return arr[dir+1];
 }
 
 class Elevator {
     public:
+
     int direction;
     int floor;
-    int prevreqsrc;
+    int prev_req_source;
     req requests[4];
     
-    // constructor sets all request data to zeroes
+    // constructor sets all data in 'requests' zeroes
     Elevator() {
         for(int i = 0; i < 4; i++) {
             for(int j = 0; j < 4; j++) {
@@ -37,29 +39,38 @@ class Elevator {
 
     // function to go to the next floor
     void next() {
-        if(floor == 1) direction = UP;
-        else if(floor == 4) direction = DOWN;
-        floor += direction;
-        printf("lift going %c, now on floor %d\n", dir_to_char(direction), floor);
-        handle_stops();
+
+        // do things if there's a pending request
+        if(pending_requests()) {
+
+            // handle passing floors
+            handle_pass();
+
+            // handle stops
+            handle_stops();
+
+            // change the direction if the elevator is on the first or last floor
+            if(floor == 1) direction = UP;
+            else if(floor == 4) direction = DOWN;
+
+            // go up/down (depends on direction)
+            floor += direction;
+
+            printf("lift going %c, now on floor %d\n", dir_to_char(direction), floor);
+
+        // else do nothing
+        } else {
+            printf("No more pending requests, the lift is at floor %d.\n", floor);
+        }
     }
 
     // functon to handle stopping
     void handle_stops() {
 
-        // check if there is a request with the current elevator floor as source
-        for(int i = 0; i < 4; i++) {
-            if(requests[i].source == floor) {
-
-                // set the request to 'passed'
-                requests[i].stop_passed = 1;
-            }
-        }
-
         // check if there's a (passed) stop request or request to
         // go in the current direction of the elevator
         if(requests[floor].type == direction ||
-          (requests[floor].type == STOP &&
+           (requests[floor].type == STOP &&
             requests[floor].stop_passed)) {
             
             // handle the request
@@ -68,16 +79,67 @@ class Elevator {
         }
     }
 
+    // function to handle passing a floor
+    void handle_pass() {
+
+        // check if there is a request with the current elevator floor as source
+        for(int i = 0; i < 4; i++) {
+            if(requests[i].source == floor && requests[i].type == STOP) {
+
+                // set the request to 'passed'
+                requests[i].stop_passed = 1;
+            }
+        }
+    }
+
     // function to make a request
     void make_request(int floor, int dir) {
-        requests[floor].type = dir;
+
+        // check if there is no stop request (stop requests can't be overridden)
+        if(requests[floor].type != STOP) {
+            requests[floor].type = dir;
+        }
 
         // if the request is a stop request, set the source as the floor from
         // the previous request
         if(dir == STOP) {
-            requests[floor].source = prevreqsrc;
+            requests[floor].source = prev_req_source;
         }
-        prevreqsrc = floor;
+        requests[floor].stop_passed = 0;
+        prev_req_source = floor;
+    }
+
+    // check if there are any pending requests
+    int pending_requests() {
+
+        // iterate through all requests
+        for(int i = 0; i < 4; i++) {
+
+            // check if the request is not 0 (NONE)
+            if(requests[i].type) {
+                //printf("Pending request: %d at floor %d\n", requests[i].type, i);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    // function to simulate iterations
+    void simulate_iterations(int num) {
+        for(int i = 0; i < num; i++) {
+            next();
+            //printf("\n");
+        }
+    }
+
+    // update the direction, this should always be called after manually
+    // setting the floor
+    void set_direction() {
+        if(floor == 4) {
+            direction = DOWN;
+        } else {
+            direction = UP;
+        }
     }
 };
 
@@ -89,12 +151,7 @@ int main() {
     // if no floor is sending a detection, go (up/down?) until a floor is sending a detection
     elevator.floor = 1;
 
-    // start by going down if the current floor is 4, else start by going up
-    if(elevator.floor == 4) {
-        elevator.direction = DOWN;
-    } else {
-        elevator.direction = UP;
-    }
+    elevator.set_direction();
 
     // floor 1 wants to go to floor 3
     elevator.make_request(1, UP);
@@ -105,7 +162,12 @@ int main() {
     elevator.make_request(2, STOP);
 
     // simulate some iterations
-    for(int i = 0; i < 10; i++) {
-        elevator.next();
-    }
+    elevator.simulate_iterations(10);
+
+    // make a new request
+    elevator.make_request(4, DOWN);
+    elevator.make_request(2, STOP);
+
+    // simulate some more iterations
+    elevator.simulate_iterations(10);
 }
